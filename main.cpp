e7ce720776e675371b46a1fa66a924592243a17a
Brett Hamilton <bretthamilton024@gmail.com>
2025-10-05 17:39:36 -0400

    intial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+int main() {
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW\n";
+	}
+
+	// MacOS requires explicitly requesting an OpenGL version and core profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on MacOS
+	
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Hello", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	// Main render loop
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
